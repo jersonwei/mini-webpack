dba268dda4897a8a1ca8af1295e8c37a451f54a1
王伟 <872937094@qq.com>
2022-05-13 21:31:04 +0800

    readFile ==>  parse ast ==> use traverse

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+import fs from 'fs'
+import parser from '@babel/parser'
+import traverse from '@babel/traverse'
+
+console.log(traverse)  // 了解traverse的内容
 function createAsset(){
-
-
-
-
-
-
-    return {
-        
-    }
-
-
-
+    // TODO 1. 获取文件内容
+    // AST 抽象语法树
     
     
+    const source =fs.readFileSync("./example/main.js",{
+        // 将babel转译成字符串
+        encoding:"utf-8"
+    })
     
-    // TODO 获取文件内容
-    // 获取依赖关系
+    console.log(source)
+    // 2.获取依赖关系
+
+    // 使用babel内置的API将读取的文件内容转译成AST树
+    const ast = parser.parse(source,{
+        sourceType:"module"
+    })
+    // console.log(ast)
+
+    traverse.default(ast,{
+        ImportDeclaration(data){
+            console.log("import==================")
+            console.log(data)
+        }
+    })
+
+
+
+
+
+    return {}
 }
+
+createAsset()
