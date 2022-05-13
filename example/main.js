aaa9578b3d1ee5ecdd1d677787dcb4a8baef5cb1
王伟 <872937094@qq.com>
2022-05-13 21:07:53 +0800

    file init / add module type in packagejson

---
+++ example/main.js
@@ -0,0 +1,5 @@
+import foo from './foo'
+
+foo()
+
+console.log('main.js')
---
+++ index.js
@@ -0,0 +1,19 @@
+function createAsset(){
+
+
+
+
+
+
+    return {
+        
+    }
+
+
+
+    
+    
+    
+    // TODO 获取文件内容
+    // 获取依赖关系
+}
