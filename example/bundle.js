67acd8a0891335f7aea0691283db36689cb363d2
王伟 <872937094@qq.com>
2022-05-14 00:30:21 +0800

    use Self-executing function refactor bundlejs

--- example/bundle.js
+++ example/bundle.js
@@ -1,49 +1,78 @@
-function require(filePath){
-    const map = {
-        "./foo.js":foojs,
-        "./bar.js":barjs,
-        "./main.js":mainjs
-    }
-    const fn = map[filePath]
-
-    const module = {
-        exports:{
+(function(madules){
+    function require(filePath){
+        const fn = madules[filePath]
+    
+        const module = {
+            exports:{
+            }
         }
+        fn(require,module,module.exports)
+    
+        return module.exports
     }
-    fn(require,module,module.exports)
-
-    return module.exports
-}
-
-require("./main.js")
-function mainjs(require,module,exports){
-// main js
-// import foo from './foo.js'
-// import bar from './bar.js'
-const {foo} = require('./foo')
-const {bar} = require('./bar')
-foo()
-bar()
-console.log('main.js')
-
-}
-function foojs(require,module,exports){
-    // foo js
-    // import bar from './bar'
-    function foo(){
-        console.log('foo')
+    
+    require("./main.js")
+    // function mainjs(require,module,exports){
+    // // main js
+    // // import foo from './foo.js'
+    // // import bar from './bar.js'
+    // const {foo} = require('./foo')
+    // const {bar} = require('./bar')
+    // foo()
+    // bar()
+    // console.log('main.js')
+    
+    // }
+    // function foojs(require,module,exports){
+    //     // foo js
+    //     // import bar from './bar'
+    //     function foo(){
+    //         console.log('foo')
+    //     }
+    //     module.exports = {
+    //         foo
+    //     }
+    // }
+    
+    // function barjs(require,module,exports){
+    //     // import foo from './foo'
+    //     function bar(){
+    //     console.log('bar')
+    // }
+    // module.exports = {
+    //     bar
+    // }
+    // }
+})({
+    "./foo.js":function foojs(require,module,exports){
+        // foo js
+        // import bar from './bar'
+        function foo(){
+            console.log('foo')
+        }
+        module.exports = {
+            foo
+        }
+    },
+    "./bar.js":
+    function barjs(require,module,exports){
+        // import foo from './foo'
+        function bar(){
+        console.log('bar')
     }
     module.exports = {
-        foo
+        bar
     }
-}
-
-function barjs(require,module,exports){
-    // import foo from './foo'
-    function bar(){
-    console.log('bar')
-}
-module.exports = {
-    bar
-}
-}
+    },
+    "./main.js":function mainjs(require,module,exports){
+        // main js
+        // import foo from './foo.js'
+        // import bar from './bar.js'
+        const {foo} = require('./foo')
+        const {bar} = require('./bar')
+        foo()
+        bar()
+        console.log('main.js')
+        
+        }
+})
