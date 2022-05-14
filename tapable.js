297956ad26e03746609ee78d1139b2dbde540398
王伟 <872937094@qq.com>
2022-05-14 12:00:14 +0800

    qoute tapable hooks

---
+++ tapable.js
@@ -0,0 +1,13 @@
+import {SyncHook,AsyncParallelHook} from 'tapable'
+
+class Car {
+	constructor() {
+		this.hooks = {
+			accelerate: new SyncHook(["newSpeed"]),
+			brake: new SyncHook(),
+			calculateRoutes: new AsyncParallelHook(["source", "target", "routesList"])
+		};
+	}
+
+	/* ... */
+}
