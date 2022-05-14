b6d06e379661e945b31f2123ff9dd7b123fa8782
王伟 <872937094@qq.com>
2022-05-14 12:07:23 +0800

    use tapable demo to register a event and trigger the event

--- tapable.js
+++ tapable.js
@@ -10,4 +10,35 @@ class Car {
 	}
 
 	/* ... */
+
+    setSpeed(newSpeed) {
+        // following call returns undefined even when you returned values
+        // 触发事件 
+        this.hooks.accelerate.call(newSpeed);
+    };
+
+    useNavigationSystemPromise(source, target) {
+        const routesList = new List();
+        return this.hooks.calculateRoutes.promise(source, target, routesList).then((res) => {
+            // res is undefined for AsyncParallelHook
+            return routesList.getRoutes();
+        });
+    };
+
+    useNavigationSystemAsync(source, target, callback) {
+        const routesList = new List();
+        this.hooks.calculateRoutes.callAsync(source, target, routesList, err => {
+            if(err) return callback(err);
+            callback(null, routesList.getRoutes());
+        });
+    }
 }
+
+// 1 注册
+const car = new Car()
+car.hooks.accelerate.tap("test",(speed) => {
+    console.log("accelerate",speed)
+});
+
+// 2 触发
+car.setSpeed(10)
